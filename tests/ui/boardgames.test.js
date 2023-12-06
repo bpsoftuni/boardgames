a0bbcd82df252254fecd6590344455db6a6619b2
bpsoftuni <bililita@students.softuni.bg>
2023-12-06 18:56:05 +0200

    add ui tests post deploy

--- tests/ui/boardgames.test.js
+++ tests/ui/boardgames.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check boardgames page', async ({ page }) => {
-    await page.goto('http://localhost:8080/boardgames');
+    await page.goto('https://examprep-f8r3.onrender.com/boardgames');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
